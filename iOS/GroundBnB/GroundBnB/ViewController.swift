bfa1368fd8434c3cd9348832faaa37d363277ae0
Santos Gagbegnon <santosgagbegnon@gmail.com>
2020-01-16 16:43:20 -0500

    Create iOS project

---
+++ iOS/GroundBnB/GroundBnB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroundBnB
+//
+//  Created by Santos on 2020-01-16.
+//  Copyright © 2020 Santos Gagbegnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
